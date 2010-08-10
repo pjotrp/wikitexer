20d6f05a7f97869d1cafb16a740286d8da610fb1
Pjotr Prins <pjotr.public01@thebird.nl>
2010-08-10 22:59:31 +0200

    Colors

--- lib/output/html/htmlwriter.rb
+++ lib/output/html/htmlwriter.rb
@@ -17,10 +17,25 @@ class HtmlWriter
 
   def tagbox text
     write "<div class=\"sourceheader\">"
-    write text 
+    write text.capitalize
     write "</div>"
   end
 
+  def code_box paragraph, env, lang=nil
+    lang = env if lang == nil
+    write "<div class=\"source\">\n"
+    write "<div class=\"#{lang}\">\n"
+    tagbox env
+    Tempfile.open("wikitexer") do | f |
+      paragraph.each { | line | f.print line }
+      f.close
+      write `/usr/bin/source-highlight -s #{lang} -i #{f.path}`
+    end
+    # paragraph.each { | line | write line }
+    write "</div>\n"
+    write "</div>\n"
+  end
+ 
   # Writes a paragraph to stdout. When last_env contains a special 
   # environment, more can be done.
   def write_paragraph last_env, environments, paragraph
@@ -31,43 +46,19 @@ class HtmlWriter
         paragraph.each { | line | write line }
         write "</div>"
       when 'cmake'
-        write "<pre>\n"
-        tagbox last_env
-        paragraph.each { | line | write line }
-        write "</pre>\n"
+        code_box paragraph, last_env, 'shell'
       when 'perl' 
-        write "<pre>\n"
-        tagbox last_env
-        paragraph.each { | line | write line }
-        write "</pre>\n"
+        code_box paragraph, last_env
       when 'python' 
-        write "<pre>\n"
-        tagbox last_env
-        paragraph.each { | line | write line }
-        write "</pre>\n"
+        code_box paragraph, last_env
       when 'c' 
+        code_box paragraph, last_env
       when 'swig'
-        write "<pre>\n"
-        tagbox last_env
-        paragraph.each { | line | write line }
-        write "</pre>\n"
-      when 'ruby' 
-        write "<div class=\"source\">\n"
-        tagbox last_env
-        Tempfile.open("wikitexer") do | f |
-          paragraph.each { | line | f.print line }
-          f.close
-          write `/usr/bin/source-highlight -s ruby -i #{f.path}`
-        end
-        # paragraph.each { | line | write line }
-        write "</div>\n"
+        code_box paragraph, last_env, 'c'
+      when 'ruby'
+        code_box paragraph, last_env
       when 'shell'
-        write "<div class=\"verbatim\">"
-        write "<pre>\n"
-        tagbox last_env
-        paragraph.each { | line | write line }
-        write "</pre>\n"
-        write "</div>"
+        code_box paragraph, last_env
     else
       paragraph.each do | line |
         write line
@@ -93,7 +84,11 @@ HEADER
     body {font-family:'times new roman',times,serif;font-size:90%;color:#222222;background-color:#F0F8FF}
     div.verbatim { color:#8B0000; background-color: #D8BFD8; border-style:outset; }
     div.sourceheader { text-align:right; color:blue; background-color:#CCCCFF; }
-    div.source { color: black; background-color:#CCFFFF; ; border-style:outset; }
+    div.source { color: black; background-color:white ; border-style:outset; }
+    div.ruby { color: black; background-color:#FFCCFF; ; border-style:outset; }
+    div.python { color: black; background-color:#CCFFFF; ; border-style:outset; }
+    div.perl { color: black; background-color:#CAFFD8; ; border-style:outset; }
+    div.shell { color: black; background-color:#ECD9D9; ; border-style:outset; }
     /*]]>*/
     </style>
 HEADER2
